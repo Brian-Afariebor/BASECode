f51e03934ba98f2152fba3e37c236a3001e96fb2
Brian Afariebor <baseredville000@gmail.com>
2026-06-18 10:29:43 -0500

    Improve tests

---
+++ src/docsstring_test.py
@@ -0,0 +1,18 @@
+from linker import Linker
+from parser import Parser
+from executable import Executable
+from executable import ExecutionMode
+
+file_path = "tests/HelloWorld.bc"
+
+linked_code = Linker.resolve(file_path)
+
+parsed_code = Parser.parse(linked_code)
+
+code_executable = Executable(
+    "Hello World",
+    parsed_code,
+    ExecutionMode.DOCSTRINGS
+)
+
+code_executable.run()
